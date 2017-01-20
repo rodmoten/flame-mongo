74217769c934f32870e0df82ee2fc13b0fc0a137
rodmoten <rod-i4hq@users.noreply.github.com>
2017-01-20 17:52:49 -0500

    : Retrieve entities from Flame using a circle, ellipse, or rectangle
    cleaned-up code

--- src/main/java/com/i4hq/flame/mongo/MongoFlameDAO.java
+++ src/main/java/com/i4hq/flame/mongo/MongoFlameDAO.java
@@ -41,7 +41,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.InsertManyOptions;
-import com.mongodb.client.model.Updates;
 
 /**
  * @author rmoten
